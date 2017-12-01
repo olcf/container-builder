d51f16eac60723312541d6c813a2af19d3b8dd22
atj <adambsimpson@gmail.com>
2017-12-01 13:21:28 -0500

    formatting

--- BuilderQueue/src/BuilderQueue.cpp
+++ BuilderQueue/src/BuilderQueue.cpp
@@ -3,16 +3,17 @@
 #include "Logger.h"
 #include "OpenStackBuilder.h"
 
-Reservation& BuilderQueue::enter() {
+Reservation &BuilderQueue::enter() {
     reservations.emplace_back(io_service, ReservationStatus::pending);
     return reservations.back();
 }
 
-void BuilderQueue::exit(Reservation& reservation) {
+void BuilderQueue::exit(Reservation &reservation) {
     reservation.status = ReservationStatus::complete;
 }
 
 void BuilderQueue::tick(asio::yield_context yield) {
+
     // Destroy all completed builder OpenStack instances
     for (auto &reservation : reservations) {
         if (reservation.status == ReservationStatus::complete && reservation.builder) {
@@ -22,26 +23,26 @@ void BuilderQueue::tick(asio::yield_context yield) {
     }
 
     // Remove all completed reservations
-   reservations.remove_if([](const auto& res){
-       return res.status == ReservationStatus::complete;
-   });
+    reservations.remove_if([](const auto &res) {
+        return res.status == ReservationStatus::complete;
+    });
 
     // Assign any available builders to outstanding reservations
     for (auto &reservation : reservations) {
         // When we're out of available builders break
-        if(available_builders.empty()) {
+        if (available_builders.empty()) {
             break;
-        }
-        else if (reservation.status == ReservationStatus::pending) {
+        } else if (reservation.status == ReservationStatus::pending) {
             reservation.ready(available_builders.front());
             available_builders.pop();
         }
     }
 
     // Attempt to create a new builder if there is space
-    if(available_builders.size() < max_available_builders) {
+    // Should we completely fill the builder reserve each tick?
+    if (available_builders.size() < max_available_builders) {
         auto opt_builder = OpenStackBuilder::request_create(io_service, yield);
-        if(opt_builder)
+        if (opt_builder)
             available_builders.push(opt_builder.get());
     }
 }

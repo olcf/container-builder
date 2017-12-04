33e4ea175c5f867793dbddc7a4ad5bf105220f30
Robert D. French <frenchrd@ornl.gov>
2017-12-03 21:34:54 -0500

    Test to verify that other boxen are not being clobbered

---
+++ tests/integration/test_bring_up_queue_doesnt_clobber_unrelated_boxes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+BOOTIMG="CADES_Ubuntu16.04_v20170804_1"
+ZONE="nova"
+FLAVOR="m1.medium"
+NIC=$(openstack network show -c id --format value or_provider_general_extnetwork1)
+
+echo "# Startup dummy VM"
+DUMMY_VM=$(openstack server create                                       \
+    --image "${BOOTIMG}"                                                \
+    --flavor "${FLAVOR}"                                                \
+    --availability-zone "${ZONE}"                                       \
+    --nic net-id="${NIC}"                                               \
+    --wait                                                              \
+    -c id                                                               \
+    -f value                                                            \
+    "DummyVM");
+
+echo "# Tear down queue"
+cd Scripts
+./TearDownQueue
+
+echo "# Assert that dummy VM was unaffected"
+openstack server list | grep $DUMMY_VM
+
+echo "# Cleanup dummy VM"
+openstack server delete --wait $DUMMY_VM

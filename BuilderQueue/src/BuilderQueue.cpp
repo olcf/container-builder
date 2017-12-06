c0380ca697757f32c8680fa834c15ada1eb080bf
atj <adambsimpson@gmail.com>
2017-12-06 09:41:52 -0500

    Bad merge is bad...

--- BuilderQueue/src/BuilderQueue.cpp
+++ BuilderQueue/src/BuilderQueue.cpp
@@ -8,12 +8,17 @@ Reservation &BuilderQueue::enter() {
 }
 
 
-
 void BuilderQueue::tick(asio::yield_context yield) {
-    // Destroy all completed builder OpenStack instances
+    // Go through reservations and check for any that are completed
+    // Spin down any completed VM's and remove them from the list of active builders
     for (auto &reservation : reservations) {
         if (reservation.complete() && reservation.builder) {
-            OpenStackBuilder::destroy(reservation.builder.get(), io_service, yield);
+            asio::spawn(io_service,
+                        [&](asio::yield_context yield) {
+                            OpenStackBuilder::destroy(reservation.builder.get(), io_service, yield);
+                            //TODO check if the builder was actually destroyed
+                            active_builders--;
+                        });
         }
     }
 
@@ -22,21 +27,33 @@ void BuilderQueue::tick(asio::yield_context yield) {
         return res.complete();
     });
 
-    // Assign any available builders to outstanding reservations
+    // Assign any unused builders to outstanding reservations
     for (auto &reservation : reservations) {
-        if (available_builders.empty()) {
+        if (unused_builders.empty()) {
             break;
         }
         if (reservation.pending()) {
-            reservation.ready(available_builders.front());
-            available_builders.pop();
+            auto builder = unused_builders.front();
+            reservation.ready(builder);
+            unused_builders.pop();
+            active_builders++;
         }
     }
 
     // Attempt to create a new builder if there is space
-    if (available_builders.size() < max_available_builders) {
-        auto opt_builder = OpenStackBuilder::request_create(io_service, yield);
-        if (opt_builder)
-            available_builders.push(opt_builder.get());
+    // Requesting a new builder may take quite a bit of time so it's done in a coroutine
+    auto used_builder_count = active_builders + unused_builders.size() + outstanding_builder_requests;
+    auto total_builder_space_left = max_builders - used_builder_count;
+    auto unused_builder_space = unused_builders.size() < max_unused_builders;
+    if (total_builder_space_left > 0 && unused_builder_space > 0) {
+        asio::spawn(io_service,
+                    [&](asio::yield_context yield) {
+                        outstanding_builder_requests++;
+                        auto opt_builder = OpenStackBuilder::request_create(io_service, yield);
+                        if (opt_builder) {
+                            unused_builders.push(opt_builder.get());
+                        }
+                        outstanding_builder_requests--;
+                    });
     }
 }

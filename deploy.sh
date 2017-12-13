5d854f4f83089ba4f1bd4dc2954c1ee25250a2c5
Robert D. French <frenchrd@ornl.gov>
2017-12-13 17:06:58 -0500

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+VERSION=$1
+
+source $MODULESHOME/init/bash
+export PATH=$PATH:$MODULESHOME/bin
+
+module unload xalt PrgEnv-pgi PrgEnv-gnu PrgEnv-intel PrgEnv-cray
+module load PrgEnv-gnu
+module swap gcc gcc/7.1.0
+
+set -x
+rm -rf $HOME/.spack
+
+unset CRAYPE_VERSION
+SW_ROOT=/sw/xk6/container-builder/$VERSION
+mkdir -p $SW_ROOT
+
+SPACKROOT=$SW_ROOT/.spack
+
+if [ ! -d $SPACKROOT ]; then
+	git clone https://github.com/spack/spack.git $SPACKROOT
+fi
+
+$SPACKROOT/bin/spack repo add spack-repo/containerbuilder
+$SPACKROOT/bin/spack install "container-builder%gcc@7.1.0 ^cmake@3.9.0%gcc@4.7+ownlibs"
+
+root=$($SPACKROOT/bin/spack config get config | grep "\btcl:" | awk '{print $2}' | sed 's/^$spack/./')
+arch=$($SPACKROOT/bin/spack arch)
+mfname=$($SPACKROOT/bin/spack module find "container-builder%gcc@7.1.0")
+
+real_mf_path="$SPACKROOT/$root/$arch/$mfname"
+echo "I think the mf is at $real_mf_path"
+
+MF_ROOT=/sw/xk6/modulefiles/container-builder
+mkdir -p $MF_ROOT
+cp $real_mf_path $MF_ROOT/$VERSION
--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 echo "## Testing building with spack"
-./tests/integration/build_with_spack.sh
+hostname

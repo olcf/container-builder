2e3f39baaa2873d37aad54353b1f30bff4c168a6
atj <adambsimpson@gmail.com>
2018-01-31 14:26:51 -0500

    Remove bad directory from boost build

--- Provisioning/deploy-titan.sh
+++ Provisioning/deploy-titan.sh
@@ -27,7 +27,7 @@ cd ${TOP_LEVEL}
 rm -rf boost_build && mkdir boost_build && cd boost_build
 curl -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz -O
 tar xf boost_1_66_0.tar.gz
-cd ${TOP_LEVEL}/boost_1_66_0
+cd boost_1_66_0
 ./bootstrap.sh --with-libraries=filesystem,regex,system,serialization,thread,program_options --prefix=${SW_ROOT}
 ./b2 install || :
 rm -rf /boost_1_66_0

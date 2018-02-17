657c0f2bcab3264c26ccc9f08e0f2bc51a811532
AdamSimpson <adambsimpson@gmail.com>
2018-02-16 19:14:10 -0500

    Add keep alive to server connections

--- BuilderQueue/include/Connection.h
+++ BuilderQueue/include/Connection.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/basic_stream_socket.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/beast/websocket.hpp>
 #include "BuilderQueue.h"
@@ -14,7 +15,15 @@ namespace websocket = beast::websocket;
 class Connection : public std::enable_shared_from_this<Connection> {
 public:
     explicit Connection(tcp::socket socket, BuilderQueue &queue) : stream(std::move(socket)),
-                                                                   queue(queue) {};
+                                                                   queue(queue) {
+        // Use keep alive, which hopefully detect badly disconnected clients
+        boost::asio::socket_base::keep_alive keep_alive(true);
+        boost::system::error_code ec;
+        socket.set_option(keep_alive, ec);
+        if(ec) {
+            Logger::error("Error setting keep alive on socket");
+        }
+    };
 
     ~Connection() {
         if (builder) {
